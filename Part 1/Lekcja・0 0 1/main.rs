bdbd32e558e6e5daa221ffa0d9ce032ee1a63c9b
coffdage <antoni.f.jasinski@gmail.com>
2026-06-19 18:10:27 +0200

    Update main.rs

--- Part 1/Lekcja・0 0 1/main.rs
+++ Part 1/Lekcja・0 0 1/main.rs
@@ -8,10 +8,3 @@ fn main() {
     print!("print!() printuje text prosty nie formatowany, bez entera automatycznego. Pojawia się pod eprint!().");
     println!(" ");
 }
-
-// Wynik programu:
-
-// println!() printuje text formatowany i z enterem automatycznie.
-// eprintln!() printuje text formatowany i z enterem automatycznie. Nakłada się nad print().
-// eprint!() printuje text prosty nie formatowany, bez entera automatycznego. Nakłada się nad print!() ale jest pod eprintln!().
-// print!() printuje text prosty nie formatowany, bez entera automatycznego. Pojawia się pod eprint!().
