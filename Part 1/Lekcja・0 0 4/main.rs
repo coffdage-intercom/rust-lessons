6569a5bc0bcae48ce5be1b821581a1296536d1f9
NeiTi4Dot <ID+username@users.noreply.github.com>
2026-08-16 16:25:44 +0200

    Cleaning repo

--- Part 1/Lekcja・0 0 4/main.rs
+++
@@ -1,9 +0,0 @@
-// Wylistowanie fruits oraz jedno po drugim osobno:
-
-fn main() {
-    let fruits: [&str; 3] = ["apple", "orange", "banana"];
-    println!("All fruits list: {:?}", fruits);
-    println!("Fruit 1: {}", fruits[0]);
-    println!("Fruit 2: {}", fruits[1]);
-    println!("Fruit 3: {}", fruits[2]);
-}
